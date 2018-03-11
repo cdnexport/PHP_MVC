add51fce40e6bb8d2f5a766f9a146991dd8da470
cdnexport <fudgemaster3000@hotmail.com>
2018-03-11 00:05:54 -0600

    Fixed 404 page markup

--- app/controllers/notfound.php
+++ app/controllers/notfound.php
@@ -1,8 +1,7 @@
 <?php
 class notfound extends Controller{
 	public function index(){
-		$links = ['<link rel="stylesheet" href="'.ASSET_ROOT . '/css/global.css'];
-			
+		$links = ['<link rel="stylesheet" href="'.ASSET_ROOT . '/css/global.css'.'"/>'];
 		$this->view('templates/header', ['links' => $links]);
 		$this->view('errors/notfound');
 		$this->view('templates/footer');
