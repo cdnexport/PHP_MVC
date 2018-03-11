631635982589a78566f0e4f922b552b673585f4f
cdnexport <fudgemaster3000@hotmail.com>
2018-03-11 09:43:18 -0500

    Register now only appears for logged out users

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -14,10 +14,10 @@
 	<ul>
 		<?php if(!isset($_SESSION['username'])): ?>
 			<li><a href="<?= ASSET_ROOT .'/login'?>">Login</a></li>
+			<li><a href="<?= ASSET_ROOT . '/register'?>">Register</a></li>
 		<?php else: ?>
 			<li><a href="#" id="user"><?= $_SESSION['username'] ?></a></li>
 			<li><a href="<?= ASSET_ROOT .'/logout'?>" id="logout">Logout</a></li>
 		<?php endif; ?>
-		<li><a href="<?= ASSET_ROOT . '/register'?>">Register</a></li>
 		<li><a href="<?= ASSET_ROOT . '/home'?>">Home</a></li>
 	</ul>
