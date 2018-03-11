9b7c6a35ea6b6f0768af22d71e375f5cb8b0580c
cdnexport <fudgemaster3000@hotmail.com>
2018-03-11 00:15:39 -0600

    Removed junk lines

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,19 +1,10 @@
 <?php 
 	class Home extends Controller{
 		public function index($user = null){
-			//echo $name . " ";
-			// $userModel = $this->model('User');
-
-			// if($user){
-			// 	$user = $userModel->get($user);
-			// }
-			// var_dump($user);
 			$links = ['<link rel="stylesheet" href="'.ASSET_ROOT . '/css/global.css'.'"/>'];
 			$title = "Home";
 			
 			$this->view('templates/header', ['links' => $links, 'title'=>$title]);
-
-			// $this->view('home/index', ['user'=>$user]);
 			$this->view('home/index');
 			$this->view('templates/footer');
 		}
